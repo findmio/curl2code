57410c0be942eb21225802e6e8b0416f4510035d
findmio <findmio@foxmail.com>
2024-03-28 14:29:30 +0800

    feat: 配置 gp 文件路径

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,45 +1,48 @@
-const { pluginReact } = require("@rsbuild/plugin-react");
+const { pluginReact } = require('@rsbuild/plugin-react');
+
+const assetPrefix = process.env.PUBLIC_RUN_MODE === 'web' ? 'curl2code' : '/';
 
 module.exports = {
     html: {
-        template: "./public/index.html",
+        template: './public/index.html',
     },
     plugins: [pluginReact()],
     source: {
         alias: {
-          '@icons': './src/icons',
+            '@icons': './src/icons',
         },
     },
     tools: {
         rspack: {
             resolve: {
                 fallback: {
-                    path: require.resolve("path-browserify"),
+                    path: require.resolve('path-browserify'),
                     fs: false,
-                    stream: require.resolve("stream-browserify"),
-                    util: require.resolve("util/"),
-                    url: require.resolve("url/"),
-                    querystring: require.resolve("querystring-es3"),
-                    string_decoder: require.resolve("string_decoder/"),
-                    buffer: require.resolve("buffer/"),
+                    stream: require.resolve('stream-browserify'),
+                    util: require.resolve('util/'),
+                    url: require.resolve('url/'),
+                    querystring: require.resolve('querystring-es3'),
+                    string_decoder: require.resolve('string_decoder/'),
+                    buffer: require.resolve('buffer/'),
                 },
             },
         },
     },
     output: {
+        assetPrefix: assetPrefix,
         distPath: {
-            js: "",
-            css: "",
+            js: '',
+            css: '',
         },
-        legalComments: "none",
+        legalComments: 'none',
         copy: {
             patterns: [
-                "public/logo.jpg",
-                "public/plugin.json",
-                "node_modules/highlight.js/styles/a11y-dark.css",
-                "node_modules/highlight.js/styles/a11y-light.css",
-                "node_modules/web-tree-sitter/tree-sitter.wasm",
-                "node_modules/curlconverter/dist/tree-sitter-bash.wasm",
+                'public/logo.jpg',
+                'public/plugin.json',
+                'node_modules/highlight.js/styles/a11y-dark.css',
+                'node_modules/highlight.js/styles/a11y-light.css',
+                'node_modules/web-tree-sitter/tree-sitter.wasm',
+                'node_modules/curlconverter/dist/tree-sitter-bash.wasm',
             ],
         },
     },
